1a3259b49cafe8f8c706f6e47e4d7bfd3dc44f2a
EPPI <109323250+EPPI-c@users.noreply.github.com>
2025-04-20 23:30:20 -0300

    chrome support

--- background.js
+++ background.js
@@ -1,12 +1,2 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    return {
-      redirectUrl: chrome.runtime.getURL("libs/awesomplete.js")
-    };
-  },
-  {
-    urls: ["https://animemusicquiz.com/scripts/pages/gamePage/libraries/awesomplete.min.js"],
-    types: ["script"]
-  },
-  ["blocking"]
-);
+console.log("Extension service worker loaded.");
+
